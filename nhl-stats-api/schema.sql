d2da2756ac0fad545f025e5c6995d77766b7699e
Tor Kingdon <tor@hearkitty.com>
2022-01-12 16:47:07 -0700

    little more setup

---
+++ nhl-stats-api/schema.sql
@@ -0,0 +1,36 @@
+DROP TABLE users IF EXISTS;
+DROP TABLE players IF EXISTS;
+-- only contians player name and id for searching on APIs
+DROP TABLE users_players IF EXISTS;
+DROP TABLE teams IF EXISTS;
+DROP TABLE users_teams IF EXISTS;
+DROP TABLE preferences IF EXISTS;
+DROP TABLE users_preferences IF EXISTS;
+
+CREATE TABLE users (
+
+);
+
+CREATE TABLE players (
+
+);
+
+CREATE TABLE users_players (
+
+);
+
+CREATE TABLE teams (
+
+);
+
+CREATE TABLE users_teams (
+
+);
+
+CREATE TABLE preferences (
+
+);
+
+CREATE TABLE users_preferences (
+
+);
