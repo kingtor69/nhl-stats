f07fa630872d085a72bd91b5c59bb75ba3e86c12
Tor Kingdon <tor@hearkitty.com>
2022-01-26 15:55:39 -0700

    mapping out component tree

---
+++ nhl-stats-app/src/AAComponentTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Child from './Child';
+
+function Component() {
+  return (
+    <div className="Component">
+    </div>
+  );
+}
+
+export default Component;
--- nhl-stats-app/src/App.js
+++ nhl-stats-app/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import './startbootstrap-grayscale-gh-pages/css/styles.css';
+// that doesn't actually work the way I was hoping
+// check out `https://react-bootstrap.github.io/getting-started/introduction/`
 import Navbar from './Navbar';
-
+import UserBlock from './UserBlock';
+import NhlStats from './NhlStats';
 
 function App() {
+  const [UserBlockPage, setUserBlockPage] = useState(false)
   return (
     <div className="App">
       <Navbar />
+      { UserBlockPage 
+        ? <UserBlock />
+        : <NhlStats />
+      }
     </div>
   );
 }
