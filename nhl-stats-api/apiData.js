a95390e88f66ba7689dc76de0b9b4c7d1533c5e6
Tor Kingdon <kingtor@gmail.com>
2022-04-06 17:57:53 -0600

    moved all py data to js files, delete py files

---
+++ nhl-stats-api/apiData.js
@@ -0,0 +1,18 @@
+
+const apiData = () => {
+    const today=Date();
+    const startYear = today.getMonth() > 8 ? today.getYear() : today.getYear() - 1;
+    const thisSeason = `{startYear}{startYear + 1}`;
+    const lastSeason = `{startYear-1}{startYear}`;
+    return {
+        statsApiNHLBaseUrl:  "https://statsapi.web.nhl.com/",
+        statsApiNhlSeasonStatsEndpoint: "/stats=statsSingleSeason&season=",
+        thisSeason,
+        lastSeason,
+        statsApiNhlTeamsSuffix: 'f',
+        // returns all teams or a team with ID added to URL
+        statsApiNhlTeamRoster: '?expand=team.roster'
+    };
+};
+
+export default apiData;
