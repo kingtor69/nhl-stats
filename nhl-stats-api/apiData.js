d25db912a8055d054a7df2889ca45d51722c8704
Tor Kingdon <kingtor@gmail.com>
2022-04-11 21:27:11 -0600

    fixed apiData

--- nhl-stats-api/apiData.js
+++ nhl-stats-api/apiData.js
@@ -1,18 +1,20 @@
 
-const apiData = () => {
-    const today = new Date();
-    const startYear = today.getMonth() > 8 ? today.getYear() : today.getYear() - 1;
-    const thisSeason = `{startYear}{startYear + 1}`;
-    const lastSeason = `{startYear-1}{startYear}`;
-    return {
-        statsApiNHLBaseUrl:  "https://statsapi.web.nhl.com/",
-        statsApiNhlSeasonStatsEndpoint: "/stats=statsSingleSeason&season=",
-        thisSeason,
-        lastSeason,
-        statsApiNhlTeamsSuffix: 'f',
-        // returns all teams or a team with ID added to URL
-        statsApiNhlTeamRoster: '?expand=team.roster'
-    };
+const today = new Date();
+const startYear = today.getMonth() > 8 ? today.getYear() : today.getYear() - 1;
+const thisSeason = `{startYear}{startYear + 1}`;
+const lastSeason = `{startYear-1}{startYear}`;
+const statsApiNHLBaseUrl:  "https://statsapi.web.nhl.com/";
+const statsApiNhlSeasonStatsEndpoint = "/stats=statsSingleSeason&season=";
+const statsApiNhlTeamsSuffix = 'f';
+const statsApiNhlTeamRoster = '?expand=team.roster';
+
+const apiData = {
+    statsApiNHLBaseUrl,
+    statsApiNhlSeasonStatsEndpoint,
+    thisSeason,
+    lastSeason,
+    statsApiNhlTeamsSuffix,
+    statsApiNhlTeamRoster
 };
 
-export default apiData;
+module.exports = apiData;
